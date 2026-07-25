723116086b8410a1526cf50b6ad859ef5ca20eb9
Kartikey Rauniyar <kartikeyrauniyar9@gmail.com>
2026-07-26 01:07:08 +0530

    Added new file and updated code

---
+++ index.js
@@ -0,0 +1,107 @@
+let data = [
+  {
+    image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC,e-usm-2-2-0.5-0.008:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@like_202006280402.png,lx-24,ly-617,w-29,l-end:l-text,ie-NjlLKyBMaWtlcw%3D%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end:l-image,i-discovery-catalog@@icons@@pt-v1.png,t-false,lfo-top_right,lx-N5,ly-0,w-160,l-end/et00478378-etdvrxwvxz-portrait.jpg",
+    movieName: "Jan Neta",
+    ageLimit: "A",
+    language: "Hindi",
+    price: 100
+  },
+  {
+    image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC,e-usm-2-2-0.5-0.008:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@like_202006280402.png,lx-24,ly-617,w-29,l-end:l-text,ie-ODM0SysgTGlrZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00447840-xckkhurxjp-portrait.jpg",
+    movieName: "Spider-Man: Brand New Day",
+    ageLimit: "UA13+",
+    language: "Hindi, English, Tamil, Telugu",
+    price: 100
+  },
+  {
+    image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC,e-usm-2-2-0.5-0.008:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OS4yLzEwICA2Ni44SysgVm90ZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00452034-qrgdyxqlhb-portrait.jpg",
+    movieName: "The Odyssey",
+    ageLimit: "A",
+    language: "English, Hindi, Tamil",
+    price: 100
+  },
+  {
+    image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC,e-usm-2-2-0.5-0.008:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-Ny45LzEwICAyNi44SysgVm90ZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00496605-fvycsspxld-portrait.jpg",
+    movieName: "Evil Dead Burn",
+    ageLimit: "A",
+    language: "English, Hindi, Tamil",
+    price: 100
+  },
+  {
+    image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC,e-usm-2-2-0.5-0.008:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OC42LzEwICA2LjhLKyBWb3Rlcw%3D%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00487783-qqxsrgnxvv-portrait.jpg",
+    movieName: "Minions & Monsters",
+    ageLimit: "UA 7+",
+    language: "English, Hindi",
+    price: 100
+  },
+  {
+    image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC,e-usm-2-2-0.5-0.008:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OC4zLzEwICAyLjRLKyBWb3Rlcw%3D%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00472951-nzgzdpkuzf-portrait.jpg",
+    movieName: "Moana",
+    ageLimit: "UA 7+",
+    language: "English, Hindi",
+    price: 100
+  },
+  {
+    image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC,e-usm-2-2-0.5-0.008:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@like_202006280402.png,lx-24,ly-617,w-29,l-end:l-text,ie-MS4zTSsgTGlrZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00430817-jxxxqxvhvj-portrait.jpg",
+    movieName: "Jana Nayagan",
+    ageLimit: "A",
+    language: "Tamil",
+    price: 100
+  },
+  {
+    image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC,e-usm-2-2-0.5-0.008:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-Ny40LzEwICAyMyBWb3Rlcw%3D%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end:l-image,i-discovery-catalog@@icons@@re-release-tag.png,lx-N6,t-false,lfo-top_right,w-180,l-end/et00505887-kkspyrhjty-portrait.jpg",
+    movieName: "Mein to Palavade Bandi Prit",
+    ageLimit: "U",
+    language: "Gujarati",
+    price: 100
+  }
+]
+
+let container = document.getElementById("container");
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function createCard(movie) {
+  let card = document.createElement("div");
+  card.setAttribute("class", "movie");
+  let movieImage = document.createElement("img");
+  movieImage.setAttribute("src", movie.image)
+  let movieName = document.createElement("h2");
+  movieName.innerText = movie.movieName;
+  let ageLimit = document.createElement("p");
+  ageLimit.innerText = movie.ageLimit;
+  let language = document.createElement("p");
+  language.innerText = movie.language;
+  let price = document.createElement("p");
+  price.innerText = `price : ${movie.price}`
+
+  let viewMore = document.createElement("button");
+  viewMore.innerText = "VIEW MORE";
+
+  let addToCart = document.createElement("button");
+  addToCart.innerText = "Add To Cart";
+
+  viewMore.addEventListener("click", ()=>{
+    console.log(movie);
+
+    console.log(JSON.stringify(movie));
+
+
+    localStorage.setItem("movie", JSON.stringify(movie));
+
+    window.location.href = "https://meet.google.com/home";
+  })
+
+  addToCart.addEventListener("click", ()=>{
+    cart.push(movie);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    console.log(cart);
+  })
+
+  card.append(movieImage, movieName, ageLimit, language,price, viewMore, addToCart);
+  container.append(card);
+}
+
+
+for(let i=0; i<data.length; i++){
+  createCard(data[i]);
+}
